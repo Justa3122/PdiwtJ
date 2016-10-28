2f00a955c16206daf12fe1b7335def4838042909
Justyna Jasek <justyna.jasek.93@gmail.com>
2016-10-28 14:22:54 +0200

    1 zadanie z mavenem

---
+++ helloworld-maven/src/main/java/com/pdiwtj/helloworld_maven/App.java
@@ -0,0 +1,13 @@
+package com.pdiwtj.helloworld_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
